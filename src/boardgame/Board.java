307bb3d7ce83d2c3a65eff5a50809c1e186903b8
Diego Sousa <diegomarcelosou@gmail.com>
2021-11-28 21:25:53 -0300

    Implementação da classe e pacote de exceções. Tratamento de possíveis erros no programa e aplicação do conceito de 'programação defensiva'.

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,6 +8,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
+		if(rows < 1 || columns < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -31,18 +34,46 @@ public class Board {
 	
 //	Retorna a matriz das peças
 	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 	
 //	Sobrecarga do Método Piece de Peças
 	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
-	
 //	Método para colocar uma peça, atribui uma posição a ela e tira do valor null
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position " + position);
+		}
+		
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+//	Analisa uma posição de acordo com a linha e coluna
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0  && column < columns;
+		
+	}
+	
+//	Analisa uma posição por meio de uma procura no tabuleiro
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+//	Confirma se a posição representa uma peça ou não
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position) != null;
 	}
 }
--- src/chessgame/ChessMatch.java
+++ src/chessgame/ChessMatch.java
@@ -36,7 +36,7 @@ public class ChessMatch {
 	
 //	Método de organização e colocação inicial das peças
 	private void initialSetup(){
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
 		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
 		board.placePiece(new King(board, Color.BLACK), new Position(7, 4));
 	}
