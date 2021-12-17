a7732ebf64d1b2ce172070464d27e9fe6a7f63be
Diego Sousa <diegomarcelosou@gmail.com>
2021-12-17 11:59:57 -0300

    Primeiros passos com o contador de movimentos

--- src/chessgame/ChessPiece.java
+++ src/chessgame/ChessPiece.java
@@ -7,6 +7,7 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece{
 	
 	private Color color;
+	private int moveCount;
 	
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -18,8 +19,20 @@ public abstract class ChessPiece extends Piece{
 		return color;
 	}
 	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
+	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		moveCount--;
 	}
 
 	protected boolean isThereOpponentPiece(Position position) {
